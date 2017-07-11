bf3ee853d1307716e39150a7c2c4c5951effaf52
adriantodt <adriantodt@outlook.com>
2017-07-11 11:28:07 -0300

    No, this is not owned by Oracle.

--- src/main/java/xyz/avarel/aje/bytecode/serialization/DataOutputConsumer.java
+++ src/main/java/xyz/avarel/aje/bytecode/serialization/DataOutputConsumer.java
@@ -1,27 +1,3 @@
-/*
- * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
 package xyz.avarel.aje.bytecode.serialization;
 
 import java.io.DataOutput;
@@ -30,7 +6,6 @@ import java.util.Objects;
 
 @FunctionalInterface
 public interface DataOutputConsumer {
-
     void writeInto(DataOutput output) throws IOException;
 
     default DataOutputConsumer andThen(DataOutputConsumer after) {
@@ -40,5 +15,4 @@ public interface DataOutputConsumer {
             after.writeInto(t);
         };
     }
-
 }
