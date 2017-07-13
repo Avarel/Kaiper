b89484520886e479345e682ce3bde4dc9c2a4e42
adriantodt <adriantodt@outlook.com>
2017-07-13 12:25:12 -0300

    Added the :all module

---
+++ vm/src/main/java/xyz/avarel/aje/AJEVM.java
@@ -0,0 +1,17 @@
+package xyz.avarel.aje;
+
+public class AJEVM {
+    public void run(byte[] bytecode) {
+        //TODO (@AdrianTodt)
+        /*
+        Specification:
+            - No dependency on AST
+            - Bytecode made by :compiler (Requires change)
+            - Stack-based
+            - Implementation like the JVM itself
+        Requirements:
+            - Non-AST Bytecode (On work)
+            - Class System (cc @Avarel)
+         */
+    }
+}
